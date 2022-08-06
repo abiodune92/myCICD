2a7c6a8795175d4d76cf3f391b5e449df1eb1f0f
abiodune92 <abiodune92@gmail.com>
2022-08-06 23:13:00 +0000

    2nd commit

---
+++ s3.tf
@@ -0,0 +1,10 @@
+resource "aws_s3_bucket" "emmanuel122" {
+  bucket_prefix = var.bucket_prefix
+  acl = var.acl
+  
+   versioning {
+    enabled = var.versioning
+  }
+  
+  tags = var.tags
+}
---
+++ variables.tf
@@ -0,0 +1,51 @@
+variable "aws_region" {
+       description = "The AWS region to create things in." 
+       default     = "us-east-1" 
+}
+
+variable "key_name" { 
+    description = " SSH keys to connect to ec2 instance" 
+    default     =  "Github" 
+}
+
+variable "instance_type" { 
+    description = "instance type for ec2" 
+    default     =  "t2.micro" 
+}
+
+variable "security_group" { 
+    description = "Name of security group" 
+    default     = "MercyGuitar" 
+}
+
+variable "tag_name" { 
+    description = "Tag Name of for Ec2 instance" 
+    default     = "Jenkins_Slave" 
+} 
+variable "ami_id" { 
+    description = "AMI for Ubuntu Ec2 instance" 
+    default     = "ami-052efd3df9dad4825" 
+}
+variable "versioning" {
+    type        = bool
+    description = "(Optional) A state of versioning."
+    default     = true
+}
+variable "acl" {
+    type        = string
+    description = " Defaults to private "
+    default     = "private"
+}
+variable "bucket_prefix" {
+    type        = string
+    description = "(required since we are not using 'bucket') Creates a unique bucket name beginning with the specified prefix"
+    default     = "emmanuel122"
+}
+variable "tags" {
+    type        = map
+    description = "(Optional) A mapping of tags to assign to the bucket."
+    default     = {
+        environment = "DEV"
+        terraform   = "true"
+    }
+}
